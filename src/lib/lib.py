3f2d91dd249b5a1ba24d59afddfd0f5665d6331c
Marina <marinalara2812@gmail.com>
2024-03-27 21:30:33 +0100

    klasa izraz

---
+++ src/lib/lib.py
@@ -0,0 +1,34 @@
+
+
+class Израз:
+    def __init__(self, ceo, r, p, n, nula):
+        self._ceo=ceo
+        self._realan=r
+        self._pozitivan=p
+        self._negativan = n
+        self._nula = nula
+    def __neg__(self):
+        pass
+        #return Neg.simplify(self)
+    def __eq__(self, b):
+        pass
+        #return self==b?
+    
+    def __add__(self, b):
+        pass
+        #return Add,.simplify(self, b)
+    
+    def __sub__(self, b):
+        pass
+        #return Sub.simplify(self, b)
+    
+    def simplify():
+        return
+    
+    def collect():
+        return
+    
+    def __str__(self):
+        return
+    
+    
