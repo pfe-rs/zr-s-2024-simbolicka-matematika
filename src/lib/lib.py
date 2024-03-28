c2f873fd30d965d4d386c23be06f6053514da40f
Nemanja Obradovic <sstefan.kv748@gmail.com>
2024-03-28 19:40:28 +0100

    klasa add

--- src/lib/lib.py
+++ src/lib/lib.py
@@ -48,3 +48,73 @@ class Broj(Izraz):  # podrazumevano je realan
     def __str__(self):
         return f"{str(self._vrednost)}"
 
+
+class Add(Izraz):
+    def __init__(self, a, b):  # a + b
+        self.lista = [a, b]
+
+        ceo = None
+        realan = None
+        pozitivan = None
+        negativan = None
+        nula = None
+        
+        if a._ceo and b._ceo:
+            ceo = True
+
+        if a._ceo and b._realan:
+            realan = True
+
+        if a._realan and b._ceo:
+            realan = True
+
+        if a._realan and b._realan:
+            realan = True
+
+        if a._pozitivan and b._pozitivan:
+            pozitivan = True
+
+        if a._negativan and b._negativan:
+            negativan = True
+
+        if a._nula and b._nula:
+            nula = True
+
+        if a._nula and b._ceo:
+            ceo = True
+
+        if a._nula and b._realan:
+            realan = True
+
+        if a._nula and b._negativan:
+            negativan = True
+
+        if a._nula and b._pozitivan:
+            pozitivan = True
+
+        if b._nula and a._ceo:
+            ceo = True
+
+        if b._nula and a._realan:
+            realan = True
+
+        if b._nula and a._negativan:
+            negativan = True
+
+        if b._nula and a._pozitivan:
+            pozitivan = True
+        
+        super().__init__(ceo,realan, pozitivan, negativan,nula)
+
+    def simplify(a, b):
+        if isinstance(a, Broj) and isinstance(b, Broj):
+            x = Broj(int(a._vrednost) + int(b._vrednost))
+            return x
+        return Add(a, b)
+
+    def __str__(self):
+        a = ""
+        for x in self.lista:
+            if (len(a) > 0): a = a + " + "
+            a = a + x.__str__()
+        return a 
