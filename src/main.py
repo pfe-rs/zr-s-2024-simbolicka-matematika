1dad197d190b56d6e1c3bf04375bd2a7db8dfd2a
Marina <marinalara2812@gmail.com>
2024-03-30 10:16:31 +0100

    klasa mul

--- src/main.py
+++ src/main.py
@@ -2,15 +2,5 @@ from lib.lib import *
 
 x = Simbol("x")
 y = Simbol("y")
-z  =Simbol("z")
-
-br = Broj(0)
-
-br2=Broj(0.75)
-izr=x-z+z+x+y
-
-jedan = Broj(1)
-izr1= izr.subs(x, jedan).subs(y, jedan).subs(z, jedan)
-print(izr1)
-
-print(x+y==y+x)
+zb = x+8.5
+print(zb)
